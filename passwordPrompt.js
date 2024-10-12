05d0f71f7ed37eb72818c274c82ee5dc6a22b00f
kulotskiI <ishielmishma24@gmail.com>
2024-10-12 18:06:41 +0800

    Initial commit

---
+++ passwordPrompt.js
@@ -0,0 +1,12 @@
+function userPassword( {
+    const passWord = "Ishiel";
+    let user:
+
+    do{
+        user = prompt("Enter your password");
+        if (user !== passWord) {
+            allert("Incorrect, please try again.");
+        }
+    } while (user !== PASSword);
+    allert ("Authorization Succesful.");
+})
